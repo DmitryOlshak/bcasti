a19d9f0e71646a2a9cd7e99e0b78af88545b1f77
Dmitry Olshak <dima.olshak@mail.ru>
2021-10-07 12:59:21 +0300

    added the lexer

---
+++ bcc/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace bcc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("> ");
+                var line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                    return;
+
+                var lexer = new Lexter(line);
+                while (true)
+                {
+                    var token = lexer.NextToken();
+                    if (token.Kind == SyntaxKind.EndOfFileToken)
+                        break;
+                    
+                    Console.WriteLine($"{token.Kind}: {token.Text} {token.Value}");
+                }
+                
+                if (line == "1 + 2 * 3")
+                    Console.WriteLine("7");
+                else
+                    Console.WriteLine("ERROR: Invalid expression!");
+            }
+        }
+    }
+
+    enum SyntaxKind
+    {
+        NumberToken,
+        WhiteSpaceToken,
+        PlusToken,
+        MinusToken,
+        StarToken,
+        SlashToken,
+        OpenParenthesisToken,
+        CloseParenthesisToken,
+        BadToken,
+        EndOfFileToken
+    }
+
+    class SyntaxToken
+    {
+        public SyntaxKind Kind { get; }
+        public int Position { get; }
+        public string Text { get; }
+        public object Value { get; }
+
+        public SyntaxToken(SyntaxKind kind, int position, string text, object value)
+        {
+            Kind = kind;
+            Position = position;
+            Text = text;
+            Value = value;
+        }
+    }
+
+    class Lexter
+    {
+        private readonly string _text;
+        private int _position;
+        
+        public Lexter(string text)
+        {
+            _text = text;
+        }
+
+        private char Current => _position >= _text.Length ? '\0' : _text[_position];
+
+        public SyntaxToken NextToken()
+        {
+            if (_position >= _text.Length)
+                return new SyntaxToken(SyntaxKind.EndOfFileToken, _position, "\0", null);
+            
+            if (char.IsDigit(Current))
+            {
+                var start = _position;
+                while (char.IsDigit(Current))
+                    Next();
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                int.TryParse(text, out var value);
+                return new SyntaxToken(SyntaxKind.NumberToken, start, text, value);
+            }
+
+            if (char.IsWhiteSpace(Current))
+            {
+                var start = _position;
+                while (char.IsWhiteSpace(Current))
+                    Next();
+                var length = _position - start;
+                var text = _text.Substring(start, length);
+                return new SyntaxToken(SyntaxKind.WhiteSpaceToken, start, text, null);
+            }
+
+            if (Current == '+')
+                return new SyntaxToken(SyntaxKind.PlusToken, _position++, "+", null);
+            
+            if (Current == '-')
+                return new SyntaxToken(SyntaxKind.MinusToken, _position++, "-", null);
+            
+            if (Current == '*')
+                return new SyntaxToken(SyntaxKind.StarToken, _position++, "*", null);
+            
+            if (Current == '/')
+                return new SyntaxToken(SyntaxKind.SlashToken, _position++, "/", null);
+            
+            if (Current == '(')
+                return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
+            
+            if (Current == ')') 
+                return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
+
+            return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1, 1), null);
+        }
+
+        private void Next() => _position++;
+    }
+}
