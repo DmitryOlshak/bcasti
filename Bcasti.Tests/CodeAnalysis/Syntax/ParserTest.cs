0e7b1d510ad48ba54ce8d0d27f93058d9548d21f
Dmitry Olshak <dima.olshak@mail.ru>
2021-11-16 12:09:27 +0300

    exposed SyntaxFacts.GetText()

---
+++ Bcasti.Tests/CodeAnalysis/Syntax/ParserTest.cs
@@ -0,0 +1,14 @@
+﻿using Bcasti.CodeAnalysis.Syntax;
+using Xunit;
+
+namespace Bcasti.Tests.CodeAnalysis.Syntax
+{
+    public class ParserTest
+    {
+        /*[Theory]
+        public void Parser_BinaryExpression_HonorsPrecedences(SyntaxKind op1, SyntaxKind op2)
+        {
+            
+        }*/
+    }
+}
---
+++ Bcasti.Tests/CodeAnalysis/Syntax/SyntaxFactTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Bcasti.CodeAnalysis.Syntax;
+using Xunit;
+
+namespace Bcasti.Tests.CodeAnalysis.Syntax
+{
+    public class SyntaxFactTest
+    {
+        [Theory]
+        [MemberData(nameof(GetSyntaxKindData))]
+        public void SyntaxFact_GetText_RoundTrips(SyntaxKind kind)
+        {
+            var text = SyntaxFacts.GetText(kind);
+            if (text is null) return;
+
+            var tokens = SyntaxTree.ParseTokens(text);
+            var token = Assert.Single(tokens);
+            Assert.Equal(kind, token.Kind);
+            Assert.Equal(text, token.Text);
+        }
+
+        public static IEnumerable<object[]> GetSyntaxKindData()
+        {
+            var kinds = Enum.GetValues(typeof(SyntaxKind)) as SyntaxKind[];
+            return kinds!.Select(kind => new object[] { kind });
+        }
+    }
+}
